3d53d1ffca44cb2bedd82e23d0ee27e0fb15c871
Dave Aigbe Jr <dave.aigbe@outlook.com>
2022-09-01 19:40:21 -0500

    - Organized skills and refactored styling of email form and contact section

--- components/EmailForm.jsx
+++ components/EmailForm.jsx
@@ -42,7 +42,7 @@ function ContactForm() {
                     errors={state.errors}
                 />
                 <button type="submit" disabled={state.submitting}>
-                    Submit
+                    Send
                 </button>
             </form>
         </section>
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -11,17 +11,18 @@ const Skills = () => {
             <section>
                 <ul className={styles.icon_container}>
                     <li><Icon icon="vscode-icons:file-type-reactjs" width={"90"}/></li>
-                    <li><Icon icon="logos:javascript" width={"90"}/></li>
+                    <li><Icon icon="logos:nextjs" width="90" inline={true} /></li>
+                    <li><Icon icon="cib:dot-net" width={"90"}/></li>
                     <li><Icon icon="logos:python" width={"90"}/></li>
+                    <li><Icon icon="logos:javascript" width={"90"}/></li>
                     <li><Icon icon="logos:c-sharp" width={"90"}/></li>
                     <li><Icon icon="logos:java" width={"90"}/></li>
                     <li><Icon icon="vscode-icons:file-type-html" width={"90"}/></li>
                     <li><Icon icon="vscode-icons:file-type-css" width={"90"}/></li>
                     <li><Icon icon="logos:git-icon" width={"90"}/></li>
+                    <li><Icon icon="logos:postgresql" width={"90"}/></li>
                     <li><Icon icon="logos:mysql" width={"90"}/></li>
                     <li><Icon icon="flat-color-icons:linux" width={"90"}/></li>
-                    <li><Icon icon="logos:postgresql" width={"90"}/></li>
-                    <li><Icon icon="cib:dot-net" width={"90"}/></li>
                     <li><Icon icon="logos:docker-icon" width={"90"}/></li>
                     <li><Icon icon="logos:postman-icon" width={"90"}/></li>
                 </ul>
