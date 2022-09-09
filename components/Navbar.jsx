48d118fc8be4d7f6eafe93390b69a10928e2a6d6
Dave Aigbe Jr <dave.aigbe@outlook.com>
2022-09-09 05:07:47 -0500

    - Added sliding animation for Navbar - Organized Navbar styling - Added shadows to Hero buttons and Email form - Added cleaner logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,24 +1,37 @@
-import styles from './component_styles/Navbar.module.css'
+import styles from './component_styles/Navbar.module.css';
 import Image from 'next/image';
+import {useEffect} from 'react';
 
 const Navbar = () => {
+
+    useEffect(() => {
+        let prevScrollPos = window.scrollY;
+        window.onscroll = function () {
+            let currentScrollPos = window.scrollY;
+            if (prevScrollPos > currentScrollPos) {
+                document.getElementById('navbar').style.top = '0';
+            } else {
+                document.getElementById('navbar').style.top = '-100px';
+            }
+            prevScrollPos = currentScrollPos;
+        };
+    });
+
     return (
-        <div>
-            <header className={styles.navbar} id={'navbar'}>
-                <Image className={styles.logo} src={'/images/my_logo-nobg.png'} height={120} width={170} alt={'Dave Aigbe Logo'}/>
-                <nav>
-                    <ul className={styles.nav__links}>
-                        <li><a href={'#projects'}>Projects</a></li>
-                        <li><a href={'#skills'}>Skills</a></li>
-                        <li><a href={'#about'}>About</a></li>
-                        <li><a href={'#contact'}>Contact</a></li>
-                    </ul>
-                </nav>
+        <header>
+            <nav className={styles.navbar} id={'navbar'}>
+                <Image className={styles.logo} src={'/images/my_logo-nobg-2.png'} height={60} width={120} alt={'Dave Aigbe Logo'}/>
+                <ul className={styles.nav__links}>
+                    <li><a href={'#projects'}>Projects</a></li>
+                    <li><a href={'#skills'}>Skills</a></li>
+                    <li><a href={'#about'}>About</a></li>
+                    <li><a href={'#contact'}>Contact</a></li>
+                </ul>
                 <a className={styles.nav__resume}
                    href={'https://docs.google.com/document/d/1apWZTYKOL4UszjU41XcSyal2X9Am7S23/edit?usp=sharing&ouid=105877498449267879603&rtpof=true&sd=true'}
                    target={'_blank'} rel={'noreferrer'}>Resume</a>
-            </header>
-        </div>
+            </nav>
+        </header>
     );
 };
 
