15634f7abab6f68b9c59ac4d6afd9c28d681024b
Dave Aigbe Jr <dave.aigbe@outlook.com>
2022-09-27 00:23:32 -0500

    - Simplified footer and added indication of it leading to top of page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './component_styles/Footer.module.css';
 import Image from 'next/image';
-import Link from 'next/link';
+
 
 const Footer = ({context, homePage}) => {
 
@@ -10,16 +10,21 @@ const Footer = ({context, homePage}) => {
     return (
         <div>
             <footer className={styles.footer}>
-                {!homePage
-                    &&
+                {!homePage ?
                     (
-                        <div className={styles.home_container}>
-                            <Link href="/">
-                                <a>← Home</a>
-                            </Link>
-                        </div>
-                    )}
-                <a href={'#top'}><Image src={logo} height={46} width={87} alt={'Dave Aigbe Logo'}/></a>
+                        <React.Fragment className={styles.home_container}>
+                            <Image src={logo} height={46} width={87} alt={'Dave Aigbe Logo'}/>
+                        </React.Fragment>
+                    )
+                    :
+                    (
+
+                        <React.Fragment className={styles.home_container}>
+                            <p>&uarr;</p>
+                            <a href={'#top'}><Image src={logo} height={46} width={87} alt={'Dave Aigbe Logo'}/></a>
+                        </React.Fragment>
+                    )
+                }
             </footer>
         </div>
     );
