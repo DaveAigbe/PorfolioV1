357184dcc89554a464eaf78a66e39f5fb32fa4c7
Dave Aigbe Jr <dave.aigbe@outlook.com>
2022-09-27 01:45:22 -0500

    - Removed logo button from non-homepages

--- components/AltLayout.jsx
+++ components/AltLayout.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import AltHeader from './AltHeader';
-import Footer from './Footer';
 
 const AltLayout = ({data, children}) => {
     return (
         <section>
             <AltHeader data={data}/>
             <main>{children}</main>
-            <Footer context={data}/>
         </section>
     );
 };
